fa959632d94dcbb3d9ea329fdb960edf39e7ee51
Unknown <greg.hutchinson@fcc-fac.ca>
2020-01-13 13:33:25 -0600

    Before execution

---
+++ backlog.sh
@@ -0,0 +1,137 @@
+mkdir a-has
+echo a-has > a-has/PITCHME.MD
+
+mkdir agenda
+echo agenda > agenda/PITCHME.MD
+
+mkdir agile-backlog
+echo agile-backlog > agile-backlog/PITCHME.MD
+
+mkdir agile-governance
+echo agile-governance > agile-governance/PITCHME.MD
+
+mkdir agile-mindset-cant-predict-future
+echo agile-mindset-cant-predict-future > agile-mindset-cant-predict-future/PITCHME.MD
+
+mkdir agile-mindset-updated-principles
+echo agile-mindset-updated-principles > agile-mindset-updated-principles/PITCHME.MD
+
+mkdir agile-mindset
+echo agile-mindset > agile-mindset/PITCHME.MD
+
+mkdir anti-patterns
+echo anti-patterns > anti-patterns/PITCHME.MD
+
+mkdir bduf
+echo bduf > bduf/PITCHME.MD
+
+mkdir benefits
+echo benefits > benefits/PITCHME.MD
+
+mkdir best-project-ever
+echo best-project-ever > best-project-ever/PITCHME.MD
+
+mkdir building-paper-airplanes
+echo building-paper-airplanes > building-paper-airplanes/PITCHME.MD
+
+mkdir celebrity-ordering
+echo celebrity-ordering > celebrity-ordering/PITCHME.MD
+
+mkdir complicated-vs-complex
+echo complicated-vs-complex > complicated-vs-complex/PITCHME.MD
+
+mkdir daily-scrum
+echo daily-scrum > daily-scrum/PITCHME.MD
+
+mkdir delivery-team
+echo delivery-team > delivery-team/PITCHME.MD
+
+mkdir focus
+echo focus > focus/PITCHME.MD
+
+mkdir glossary
+echo glossary > glossary/PITCHME.MD
+
+mkdir historyofagile
+echo historyofagile > historyofagile/PITCHME.MD
+
+mkdir initiatingthedesign
+echo initiatingthedesign > initiatingthedesign/PITCHME.MD
+
+mkdir introduction
+echo introduction > introduction/PITCHME.MD
+
+mkdir marble-challange
+echo marble-challange > marble-challange/PITCHME.MD
+
+mkdir marshmallowchallenge
+echo marshmallowchallenge > marshmallowchallenge/PITCHME.MD
+
+mkdir minimum-viable-product
+echo minimum-viable-product > minimum-viable-product/PITCHME.MD
+
+mkdir potentially-shippable-product-increments
+echo potentially-shippable-product-increments > potentially-shippable-product-increments/PITCHME.MD
+
+mkdir product-backlog-and-refinement
+echo product-backlog-and-refinement > product-backlog-and-refinement/PITCHME.MD
+
+mkdir product-owner
+echo product-owner > product-owner/PITCHME.MD
+
+mkdir project-vs-product
+echo project-vs-product > project-vs-product/PITCHME.MD
+
+mkdir requirements-vs-design
+echo requirements-vs-design > requirements-vs-design/PITCHME.MD
+
+mkdir resources
+echo resources > resources/PITCHME.MD
+
+mkdir retrospective
+echo retrospective > retrospective/PITCHME.MD
+
+mkdir scrum-master-guild
+echo scrum-master-guild > scrum-master-guild/PITCHME.MD
+
+mkdir scrum-master
+echo scrum-master > scrum-master/PITCHME.MD
+
+mkdir scrum-overview
+echo scrum-overview > scrum-overview/PITCHME.MD
+
+mkdir scrum-pitfalls
+echo scrum-pitfalls > scrum-pitfalls/PITCHME.MD
+
+mkdir scrum-game
+echo scrum-game > scrum-game/PITCHME.MD
+
+mkdir scrumban
+echo scrumban > scrumban/PITCHME.MD
+
+mkdir sort-it-out
+echo sort-it-out > sort-it-out/PITCHME.MD
+
+mkdir sprint-backlog
+echo sprint-backlog > sprint-backlog/PITCHME.MD
+
+mkdir sprint-planning
+echo sprint-planning > sprint-planning/PITCHME.MD
+
+mkdir sprint-review
+echo sprint-review > sprint-review/PITCHME.MD
+
+mkdir sprint
+echo sprint > sprint/PITCHME.MD
+
+mkdir summer-meadow
+echo summer-meadow > summer-meadow/PITCHME.MD
+
+mkdir three-projects-three-expirements
+echo three-projects-three-expirements > three-projects-three-expirements/PITCHME.MD
+
+mkdir triangle
+echo triangle > triangle/PITCHME.MD
+
+mkdir tour-de-sprint
+echo tour-de-sprint > tour-de-sprint/PITCHME.MD
