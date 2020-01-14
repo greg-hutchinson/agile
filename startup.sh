cf49d239d4431ca9f01cfb55dc1cdd1a61dd5f62
Unknown <greg.hutchinson@fcc-fac.ca>
2020-01-13 19:43:05 -0600

    Fixed startup for windows as well

--- startup.sh
+++ startup.sh
@@ -1,1 +1,2 @@
-docker run -it -v /Users/hutching/git-repos/agile:/repo -p 9000:9000 gitpitch/desktop:pro;
+#docker run -it -v /Users/hutching/git-repos/agile:/repo -p 9000:9000 gitpitch/desktop:pro;
+docker run -it -v d:/gitRepos/Personal/agile:/repo -p 9000:9000 gitpitch/desktop:pro
