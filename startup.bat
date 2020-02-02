ceca1670f88b8234edec22237d5ec56a9552e176
Unknown <greg.hutchinson@fcc-fac.ca>
2020-02-02 16:33:04 -0600

    Updated to print on Recipe cards

---
+++ startup.bat
@@ -0,0 +1,4 @@
+#docker run -it -v /Users/hutching/git-repos/agile:/repo -p 9000:9000 gitpitch/desktop:pro;
+docker run -it -v d:/gitRepos/Personal/agile:/repo -p 9000:9000 gitpitch/desktop:pro
+
+https://gitpitch.com/greg-hutchinson/agile/master#/
